c1f8500602050f37ad0e9263bc187f6242cf3b09
kairzhan <13@gmail.com>
2021-09-23 20:19:52 +0600

    changed 1.py

--- 1.py
+++ 1.py
@@ -1,19 +1,35 @@
 # print("Git Git")
 
-print("введите арифметическую операцию через пробел")
+# print("введите арифметическую операцию через пробел")
 
+# print()
+
+# a,b,c = input().split()
+# a = int(a)
+# b = str(b)
+# c = int(c)
+
+# if b == "+":
+#     print(a+c)
+# elif b == "-":
+#     print(a-c)
+# elif b == "*":
+#     print(a*c)
+# elif b == "/":
+#     print(a/c)
+
+
+print("input a")
 print()
-
-a,b,c = input().split()
-a = int(a)
-b = str(b)
-c = int(c)
-
-if b == "+":
-    print(a+c)
-elif b == "-":
-    print(a-c)
-elif b == "*":
-    print(a*c)
-elif b == "/":
-    print(a/c)
+a = int(input())
+print()
+print("input b ")
+print()
+b = int(input())
+print()
+print("input c")
+print()
+c = int(input())
+print()
+d = a + b + c
+print("d =",d)
